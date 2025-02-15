854ea57028d03fc8a36c1ff86f6fae315fcf70ce
M2LTN <aaron.malton@voco.ee>
2025-02-15 17:04:00 +0200

    displayed all articles

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 
 const app = express()
-
-const path = require('path');
 // add template engine
 const hbs = require('express-handlebars');
 
@@ -33,6 +31,21 @@ con.connect((err) => {
     if (err) throw err;
     console.log('Connected to joga_mysql db')
 })
+
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index', {
+            articles: articles
+    })
+    res.render('index', {
+        articles: articles
+    })
+})})
+
 
 app.listen(3003, () => {
     console.log('App is started at http://localhost:3003')
