005c532b9a396b4a3edc3815c442cd1316dc026c
M2LTN <aaron.malton@voco.ee>
2025-02-16 14:07:11 +0200

    Created article controller and route, refractored application code

--- controllers/article.js
+++ controllers/article.js
@@ -1,30 +1,6 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
+const con = require('../utils/db');
 
-const app = express()
-// add template engine
-const hbs = require('express-handlebars');
-
-// setup template engine directory and files extensions
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.engine('hbs', hbs.engine({
-    extname: 'hbs',
-    defaultLayout: 'main',
-    layoutsDir: __dirname + '/views/layouts/',
-}));
-
-app.use(express.static('public'));
-
-app.use(bodyParser.urlencoded({ extended: true }))
-
-const articleRoutes = require('/routes/article'); //importing article route
-
-app.use ('/', articleRoutes);
-app.use('/article', articleRoutes)
-
-app.get('/', (req, res) => {
+const getAllArticles = (req, res) => {
     let query = "SELECT * FROM article";
     let articles = []
     con.query(query, (err, result) => {
@@ -36,10 +12,10 @@ app.get('/', (req, res) => {
     res.render('index', {
         articles: articles
     })
-})})
+})}
 
 // Show article by this slug
-app.get('/article/:slug', (req, res) => {
+const getArticleBySlug = (req, res) => {
     let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
     let article
     con.query(query, (err, result) => {
@@ -59,8 +35,9 @@ app.get('/article/:slug', (req, res) => {
         })
     }
     })
-});
+};
 
-app.listen(3003, () => {
-    console.log('App is started at http://localhost:3003')
-})
+module.exports={
+    getAllArticles,
+    getArticleBySlug
+};
