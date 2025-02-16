50dfa990a4f2b995e3f9cd0697e47275192b8779
M2LTN <aaron.malton@voco.ee>
2025-02-16 14:33:08 +0200

    created author and article route and controllers, refractored app code meow

--- index.js
+++ index.js
@@ -19,47 +19,12 @@ app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const articleRoutes = require('/routes/article'); //importing article route
+const articleRouter = require('./routes/article'); // import article route
+const authorRouter = require('./routes/author'); // import author route
 
-app.use ('/', articleRoutes);
-app.use('/article', articleRoutes)
+app.use('/', articleRouter);
+app.use('/author', authorRouter);
 
-app.get('/', (req, res) => {
-    let query = "SELECT * FROM article";
-    let articles = []
-    con.query(query, (err, result) => {
-        if (err) throw err;
-        articles = result
-        res.render('index', {
-            articles: articles
-    })
-    res.render('index', {
-        articles: articles
-    })
-})})
-
-// Show article by this slug
-app.get('/article/:slug', (req, res) => {
-    let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
-    let article
-    con.query(query, (err, result) => {
-        if (err) throw err;
-        article = result[0]
-        if (article.author_id) {
-            let authorQuery = `SELECT name FROM author WHERE id=${article.author_id}`;
-             con.query(authorQuery, (err, authorResult) => {
-                if (err) throw err;
-                    let authorName = authorResult.length > 0 ? 
-                    authorResult[0].name : 'Unknown Author';
-                    article.authorName = authorResult[0].name;
-                    res.render('article', {
-                        article: article,
-                        author_name: authorName
-            })
-        })
-    }
-    })
-});
 
 app.listen(3003, () => {
     console.log('App is started at http://localhost:3003')
--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'qwerty',
     database: 'joga_mysql'
 });
 
-module.exports = db;
+con.connect(err => {
+    if (err) throw err;
+    console.log('Connection established');
+});
+module.exports = con;
